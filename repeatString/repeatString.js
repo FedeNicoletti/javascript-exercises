b3a2271452f3f9aa6a9f58bc70cdcf294afc4cc4
FedeNicoletti <federiconicoletti@outlook.com>
2021-03-09 03:56:34 -0300

    Exercise repeatString done

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(hey, number) {
+ var string = hey;
+ if(number < 0) return 'ERROR';
+ if(number === 0) return '';
+ if(!hey) return '';
+ for (i = 1; i < number; i++) {
+     string = string + 'hey';
+   }
+   return string;
 }
 
 module.exports = repeatString
